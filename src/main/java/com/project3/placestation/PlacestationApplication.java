a54fb7c580b105d30546b5f44eb21659b2f84aba
Kira7124 <afc2015@naver.com>
2024-02-14 17:40:11 +0900

    placestation프로젝트생성

---
+++ src/main/java/com/project3/placestation/PlacestationApplication.java
@@ -0,0 +1,13 @@
+package com.project3.placestation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacestationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacestationApplication.class, args);
+	}
+
+}
