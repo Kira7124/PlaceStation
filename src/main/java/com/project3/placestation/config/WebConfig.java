ad4dd062e3ffb6ddc5a7d35c3b507d70f26cc738
sji8562 <whaqlzkem@naver.com>
2024-02-26 18:54:52 +0900

    2024002026 머지 완료

---
+++ src/main/java/com/project3/placestation/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.project3.placestation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public HiddenHttpMethodFilter httpMethodFilter() {
+        HiddenHttpMethodFilter hiddenHttpMethodFilter = new HiddenHttpMethodFilter();
+        return hiddenHttpMethodFilter;
+    }
+}
