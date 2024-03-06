6450a0a533ac6bfd98f068053e72c9c6b6bf618e
Kira7124 <afc2015@naver.com>
2024-03-06 16:15:43 +0900

    db더미데이터적용후확인

--- src/main/java/com/project3/placestation/admin/controller/AdminController.java
+++ src/main/java/com/project3/placestation/admin/controller/AdminController.java
@@ -63,7 +63,7 @@ public class AdminController {
 	private AdminProdHistoryService adminProdHistoryService;
 	@Autowired
 	private HttpSession httpSession;
-
+	
 	// http://localhost:80/admin/admin-main
 	// 관리자 기본메인페이지 출력
 	@GetMapping("/admin-main")
